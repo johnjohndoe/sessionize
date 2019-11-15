c42bcb30c7ac1ec9276a436ebe82e2fa9ef30fb3
Tobias Preuss <tobias.preuss@googlemail.com>
2019-11-15 10:31:13 +0100

    Use okhttp v.3.12.5.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/sessionize/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/sessionize/Dependencies.kt
@@ -21,7 +21,7 @@ object Libs {
         const val junitJupiter = "5.5.2"
         const val kotlinCoroutinesRetrofit = "1.1.0"
         const val moshi = "1.8.0"
-        const val okhttp = "3.12.3"
+        const val okhttp = "3.12.5"
         const val retrofit = "2.6.0"
         const val threetenbp = "1.4.0"
     }
